66bcd8d04cacc70e8377b0cb1b9cd0a7fbaa9e28
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-02 23:01:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
-  index = input.to_i
-  index = index -1
+def input_to_index(user_input)
+  user_index = user_input.to_i
+  user_index = user_index -1
 end
