a1f8a1fffbe61dd37b636f6433a0509a28ad162c
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-03 00:43:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,3 +51,13 @@ def turn (board)
     turn(board)
   end
 end
+
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+  end
+  return turn
+end
