d4b901d9d989f3125247a333ea61641bed5265b8
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-03 00:34:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,6 @@ def turn (board)
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
     move(board, index, character)
-    display_board(board)
   else
     print "Please enter a valid input 1-9:"
     user_input = gets.strip
